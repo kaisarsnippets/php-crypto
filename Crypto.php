8a25f35995c8af4e6f32de9110e35cb385152bad
KaisarCode <kaisar@kaisarcode.com>
2023-03-28 15:27:35 -0300

    Add comments and exclude character in bytes generation

--- Crypto.php
+++ Crypto.php
@@ -1,38 +1,58 @@
 <?php
-Class Crypto {
-    
-    // Encrypt AES + Salt + HEX
-    static function encrypt($str, $pwd){
+/**
+ * Class Crypto provides methods for encrypting and decrypting strings.
+ */
+class Crypto {
+
+    /**
+     * Encrypts a given string using AES encryption with salt and hexadecimal encoding.
+     *
+     * @param string $str The string to encrypt.
+     * @param string $pwd The password to use for encryption.
+     * @return string|false The encrypted string, or false if encryption fails.
+     */
+    public static function encrypt($str, $pwd){
         $out = false;
         try {
-        $pwd = sha1($pwd);
-        $iv  = random_bytes(16);
-        $slt = sha1(random_bytes(32));
-        $pws = hash('sha256', $pwd.$slt);
-        $mth = 'aes-256-cbc';
-        @$pwc = openssl_encrypt($str, $mth, $pws, null, $iv);
-        $out = "$iv:$slt:$pwc";
-        $out = bin2hex($out);
-        } catch (Exception $e) {}
+            $mth = 'aes-256-cbc';
+            $pwd = sha1($pwd);
+            $ivl = openssl_cipher_iv_length($mth);
+            $iv  = random_bytes($ivl);
+            $iv = str_replace(":",".",$iv);
+            $slt = sha1(random_bytes(32));
+            $pws = hash('sha256', $pwd.$slt);
+            $pwc = openssl_encrypt($str, $mth, $pws, 0, $iv);
+            $out = "$iv:$slt:$pwc";
+            $out = urlencode($out);
+            $out = bin2hex($out);
+        } catch (Exception $e) { }
         return $out;
     }
-    
-    // DECRYPT AES + SALT + HEX
-    public function decrypt($str, $pwd){
+
+    /**
+     * Decrypts a given string that was encrypted using AES encryption with salt and hexadecimal encoding.
+     *
+     * @param string $str The string to decrypt.
+     * @param string $pwd The password to use for decryption.
+     * @return string|false The decrypted string, or false if decryption fails.
+     */
+    public static function decrypt($str, $pwd){
         $out = false;
         try {
-        $pwd = sha1($pwd);
-       @$str = hex2bin($str);
-        $cmp = explode(':', $str);
-        if (count($cmp) >= 3) {
-        $iv  = $cmp[0];
-        $slt = $cmp[1];
-        $txt = $cmp[2];
-        $pws = hash('sha256', $pwd.$slt);
-        $mth = 'aes-256-cbc';
-       @$pwc = openssl_decrypt($txt, $mth, $pws, null, $iv);
-        $out = $pwc;
-        }} catch (Exception $e) {}
+            $mth = 'aes-256-cbc';
+            $pwd = sha1($pwd);
+            $str = hex2bin($str);
+            $str = urldecode($str);
+            $cmp = explode(':', $str);
+            if (count($cmp) >= 3) {
+                $iv  = $cmp[0];
+                $slt = $cmp[1];
+                $txt = $cmp[2];
+                $pws = hash('sha256', $pwd.$slt);
+                $pwc = openssl_decrypt($txt, $mth, $pws, 0, $iv);
+                $out = $pwc;
+            }
+        } catch (Exception $e) { }
         return $out;
     }
 }
